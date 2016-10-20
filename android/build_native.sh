e6306c4036a270034300a30301d02135a826c4a4
fufz <fangzhu.ffz@alibaba-inc.com>
2016-10-20 20:35:58 +0800

    Update build scripts for android.

--- android/build_native.sh
+++ android/build_native.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
 mkdir ../dist
+mkdir ../dist/arm
+mkdir ../dist/x86
 
 # for arm
 cd ..
@@ -15,9 +17,9 @@ ndk-build -B V=1 -j4 APP_ABI=armeabi-v7a
 
 ls -lR libs
 
-cp ../config.h ../dist/config_arm.h
-cp libs/armeabi-v7a/libspeex-jni.so ../dist/libspeex-jni_arm.so
-cp obj/local/armeabi-v7a/libspeex.a ../dist/libspeex_arm.a
+cp ../config.h ../dist/arm/config.h
+cp libs/armeabi-v7a/libspeex-jni.so ../dist/arm/libspeex-jni.so
+cp obj/local/armeabi-v7a/libspeex.a ../dist/arm/libspeex.a
 
 # for x86
 cd ..
@@ -32,6 +34,6 @@ ndk-build -B V=1 -j4 APP_ABI=x86
 
 ls -lR libs
 
-cp ../config.h ../dist/config_x86.h
-cp libs/x86/libspeex-jni.so ../dist/libspeex-jni_x86.so
-cp obj/local/x86/libspeex.a ../dist/libspeex_x86.a
+cp ../config.h ../dist/x86/config.h
+cp libs/armeabi-v7a/libspeex-jni.so ../dist/x86/libspeex-jni.so
+cp obj/local/armeabi-v7a/libspeex.a ../dist/x86/libspeex.a
--- android/jni/Application.mk
+++ android/jni/Application.mk
@@ -1,7 +1,7 @@
 # The ARMv7 is significanly faster due to the use of the hardware FPU
 # APP_ABI := armeabi-v7a armeabi x86 mips
 APP_ABI := armeabi-v7a
-APP_PLATFORM := android-8
+APP_PLATFORM := android-14
 
 APP_STL := gnustl_static
 
