6e04bfa884e6719b51573ed8bfa713b71fc29db6
Tristan Matthews <tmatth@videolan.org>
2018-10-21 23:01:37 -0400

    generate-samples: change output directory

--- src/generate-samples.sh
+++ src/generate-samples.sh
@@ -6,7 +6,8 @@ set -u
 MALE_SAMPLE=male.wav
 FEMALE_SAMPLE=female.wav
 WB_MALE_SAMPLE=wb_male.wav
-mkdir -p test-vectors
+OUTPUT_DIR=samples
+mkdir -p ${OUTPUT_DIR}
 
 samples="${MALE_SAMPLE} ${FEMALE_SAMPLE} ${WB_MALE_SAMPLE}"
 for sample in $samples; do
@@ -16,15 +17,15 @@ done
 bitrates="4 8 11 15"
 for b in $bitrates; do
     kbitrate=$(expr $b \* 1000)
-    ./speexenc --narrowband --bitrate ${kbitrate}  ${MALE_SAMPLE} test-vectors/male_speex_${b}.spx
-    ./speexenc --narrowband --bitrate ${kbitrate} --vbr ${MALE_SAMPLE} test-vectors/male_speex_${b}_vbr.spx
-    ./speexenc --narrowband --bitrate ${kbitrate} ${FEMALE_SAMPLE} test-vectors/female_speex_${b}.spx
-    ./speexenc --narrowband --bitrate ${kbitrate} --vbr ${FEMALE_SAMPLE} test-vectors/female_speex_${b}_vbr.spx
+    ./speexenc --narrowband --bitrate ${kbitrate}  ${MALE_SAMPLE} ${OUTPUT_DIR}/male_speex_${b}.spx
+    ./speexenc --narrowband --bitrate ${kbitrate} --vbr ${MALE_SAMPLE} ${OUTPUT_DIR}/male_speex_${b}_vbr.spx
+    ./speexenc --narrowband --bitrate ${kbitrate} ${FEMALE_SAMPLE} ${OUTPUT_DIR}/female_speex_${b}.spx
+    ./speexenc --narrowband --bitrate ${kbitrate} --vbr ${FEMALE_SAMPLE} ${OUTPUT_DIR}/female_speex_${b}_vbr.spx
 done
 
 bitrates="10 12 17 18 21 28"
 for b in $bitrates; do
     kbitrate=$(expr $b \* 1000)
-    ./speexenc --wideband --bitrate ${kbitrate}  ${WB_MALE_SAMPLE} test-vectors/wb_male_speex_${b}.spx
-    ./speexenc --wideband  --bitrate ${kbitrate} --vbr ${WB_MALE_SAMPLE} test-vectors/wb_male_speex_${b}_vbr.spx
+    ./speexenc --wideband --bitrate ${kbitrate}  ${WB_MALE_SAMPLE} ${OUTPUT_DIR}/wb_male_speex_${b}.spx
+    ./speexenc --wideband  --bitrate ${kbitrate} --vbr ${WB_MALE_SAMPLE} ${OUTPUT_DIR}/wb_male_speex_${b}_vbr.spx
 done
