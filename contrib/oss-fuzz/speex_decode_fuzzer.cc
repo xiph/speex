8808b1f0c2890143186d009040abd7a38d2af05e
Tristan Matthews <tmatth@videolan.org>
2019-02-27 12:22:58 -0500

    oss-fuzz: rename to .cc
    Apparently clang complains otherwise.
    
    Reported by Tyson Smith.

--- contrib/oss-fuzz/speex_decode_fuzzer.cc
+++ contrib/oss-fuzz/speex_decode_fuzzer.cc
@@ -1,5 +1,5 @@
 /* Copyright (C) 2017 Tristan Matthews
-   File: speex_decode_fuzzer.c
+   File: speex_decode_fuzzer.cc
 
    Redistribution and use in source and binary forms, with or without
    modification, are permitted provided that the following conditions
