xiph/speex

commit 630163d19be79c37d28b5e2d25f8a0aed669d639

nb_decode: memset innovation memory in null mode
After calling nb_decode, sb_decode is reading this memory (as `low_innov_alias`)
so in the case of returning early, we should make sure it's initialized.

Fixes msan error:
 Issue 68413: speex:speex_decode_fuzzer_fixed: Use-of-uninitialized-value in compute_rms16

found-by: continuous fuzzing process https://github.com/google/oss-fuzz/tree/master/projects/speex

Fixes https://gitlab.xiph.org/xiph/speex/-/issues/2047
Fixes https://bugs.chromium.org/p/oss-fuzz/issues/detail?id=68413

Signed-off-by: Tristan Matthews <tmatth@videolan.org>

libspeex/nb_celp.c
@@ -1472,6 +1472,10 @@ int nb_decode(void *state, SpeexBits *bits, void *vout)
       /* Final signal synthesis from excitation */
       iir_mem16(st->exc, lpc, out, NB_FRAME_SIZE, NB_ORDER, st->mem_sp, stack);
 
+      /* Normally this is written to later but since this is returning early,
+         avoid reading uninitialized memory in caller */
+      SPEEX_MEMSET(st->innov_save, 0, NB_NB_SUBFRAMES*NB_SUBFRAME_SIZE);
+
       st->count_lost=0;
       return 0;
    }

commit 117bcc047b2136cd0b29404ffc44f781b550e301

nb_celp: add missing guard before memset
Fixes: Null-dereference WRITE in nb_decode
Fixes: clusterfuzz-testcase-minimized-speex_decode_fuzzer_fixed-5091735494524928

Regression from commit 630163d19be79c37d28b5e2d25f8a0aed669d639

Found-by: continuous fuzzing process https://github.com/google/oss-fuzz/tree/master/projects/speex
Signed-off-by: Tristan Matthews <tmatth@videolan.org>

libspeex/nb_celp.c
@@ -1467,7 +1467,8 @@ int nb_decode(void *state, SpeexBits *bits, void *vout)
 
       /* Normally this is written to later but since this is returning early,
          avoid reading uninitialized memory in caller */
-      SPEEX_MEMSET(st->innov_save, 0, NB_NB_SUBFRAMES*NB_SUBFRAME_SIZE);
+      if (st->innov_save)
+         SPEEX_MEMSET(st->innov_save, 0, NB_NB_SUBFRAMES*NB_SUBFRAME_SIZE);
 
       st->count_lost=0;
       return 0;