8cf27eadb5a8eabf3b3e93e7dde2f20092119c36
Tristan Matthews <tmatth@videolan.org>
2025-01-16 20:29:33 -0500

    generate-samples: cleanup whitespace

--- src/generate-samples.sh
+++ src/generate-samples.sh
@@ -21,7 +21,7 @@ wget --no-clobber https://people.xiph.org/~tterribe/speex/${UWB_MALE_SAMPLE}
 bitrates="4 8 11 15"
 for b in $bitrates; do
     kbitrate=$(expr $b \* 1000)
-    ./speexenc --narrowband --bitrate ${kbitrate}  ${MALE_SAMPLE} ${OUTPUT_DIR}/male_speex_${b}.spx
+    ./speexenc --narrowband --bitrate ${kbitrate} ${MALE_SAMPLE} ${OUTPUT_DIR}/male_speex_${b}.spx
     ./speexenc --narrowband --bitrate ${kbitrate} --vbr ${MALE_SAMPLE} ${OUTPUT_DIR}/male_speex_${b}_vbr.spx
     ./speexenc --narrowband --bitrate ${kbitrate} ${FEMALE_SAMPLE} ${OUTPUT_DIR}/female_speex_${b}.spx
     ./speexenc --narrowband --bitrate ${kbitrate} --vbr ${FEMALE_SAMPLE} ${OUTPUT_DIR}/female_speex_${b}_vbr.spx
@@ -30,8 +30,8 @@ done
 bitrates="10 12 17 18 21 28"
 for b in $bitrates; do
     kbitrate=$(expr $b \* 1000)
-    ./speexenc --wideband --bitrate ${kbitrate}  ${WB_MALE_SAMPLE} ${OUTPUT_DIR}/wb_male_speex_${b}.spx
-    ./speexenc --wideband  --bitrate ${kbitrate} --vbr ${WB_MALE_SAMPLE} ${OUTPUT_DIR}/wb_male_speex_${b}_vbr.spx
+    ./speexenc --wideband --bitrate ${kbitrate} ${WB_MALE_SAMPLE} ${OUTPUT_DIR}/wb_male_speex_${b}.spx
+    ./speexenc --wideband --bitrate ${kbitrate} --vbr ${WB_MALE_SAMPLE} ${OUTPUT_DIR}/wb_male_speex_${b}_vbr.spx
 done
 
 bitrates="10 12 17 18 21 28"
